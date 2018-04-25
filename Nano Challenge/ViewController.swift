6b6c61beeefa09ba993ce0cd6f54fe58c4e4bcd7
Claudya Tan <claudyatan@10-61-50-150.0200.bin.id.iosda.org>
2018-04-25 12:27:49 +0700

    Initial Commit

---
+++ Nano Challenge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Nano Challenge
+//
+//  Created by Claudya Tan on 25/04/18.
+//  Copyright © 2018 Claudya Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
